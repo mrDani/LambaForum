edcff0343a89aa77c0b44e50eeb8690f87dad789
Danny <Darnnielnology@gmail.com>
2020-04-18 19:39:03 +0100

    Add service layer

---
+++ LambaForum.Data/IForum.cs
@@ -0,0 +1,19 @@
+﻿using LambaForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LambaForum.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int Id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+    }
+}
