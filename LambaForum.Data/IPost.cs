091aa05516bd7466a9160b47f51ec69c4da13a57
Danny <Darnnielnology@gmail.com>
2020-04-21 15:36:32 +0100

    Add PostService, Forum Views

--- LambaForum.Data/IPost.cs
+++ LambaForum.Data/IPost.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using LambaForum.Data.Models;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace LambaForum.Data
 {
-    interface IPost
+    public interface IPost
     {
+        Post GetById(int id);
+        IEnumerable<Post> GetAll();
+        IEnumerable<Post> GetFilteredPosts(string searchQuery);
+        IEnumerable<Post> GetPostsByForum(int id);
+
+        Task Add(Post post);
+        Task Delete(int id);
+        Task EditPostContent(int id, string newContent);
+       
     }
 }
