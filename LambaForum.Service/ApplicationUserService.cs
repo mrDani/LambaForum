5d35b66939c0ad00b40f2cec2a64fa52bef5d0f3
Danny <Darnnielnology@gmail.com>
2020-04-24 16:24:53 +0100

    Create Profile Controller, AppUser, Upload serices

---
+++ LambaForum.Service/ApplicationUserService.cs
@@ -0,0 +1,43 @@
+﻿using LambaForum.Data;
+using LambaForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambaForum.Service
+{
+    class ApplicationUserService : IApplicationUser
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationUserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<ApplicationUser> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ApplicationUser GetById(string id)
+        {
+            return GetAll().FirstOrDefault(user => user.Id == id);
+        }
+
+        public Task IncrementRating(string id, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SetProfileImage(string id, Uri uri)
+        {
+            var user = GetById(id);
+            user.ProfileImageUrl = uri.AbsoluteUri;
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
