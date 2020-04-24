e0ae46ecaf4c990e1d70becacc2f577f73281d83
Danny <Darnnielnology@gmail.com>
2020-04-24 15:03:34 +0100

    Return View to Search controller action

--- LambaForum/Controllers/SearchController.cs
+++ LambaForum/Controllers/SearchController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LambaForum.Data;
+using LambaForum.Data.Models;
+using LambaForum.Models.Forum;
+using LambaForum.Models.Post;
+using LambaForum.Models.Search;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LambaForum.Controllers
@@ -19,7 +23,44 @@ namespace LambaForum.Controllers
         public IActionResult Results(string searchQuery)
         {
             var posts = _postService.GetFilteredPosts(searchQuery);
+
+            var areNoResults = (!string.IsNullOrEmpty(searchQuery) && !posts.Any());
+
+            var postListings = posts.Select(post => new PostListingModel
+            {
+                Id = post.Id,
+                AuthorId = post.User.Id,
+                AuthorName =post .User.UserName,
+                AuthorRating = post.User.Rating,
+                Title = post.Title,
+                DatePosted =post.Created.ToString(),
+                RepliesCount = post.Replies.Count(),
+                Forum = BuildForumListing(post)
+            });
+
+            var model = new SearchResultModel
+            {
+                Posts = postListings,
+                SearchQuery = searchQuery,
+                EmptySearchResults = areNoResults
+            };
+
+            return View(model);
         }
+
+        private ForumListingModel BuildForumListing(Post post)
+        {
+            var forum = post.Forum;
+
+            return new ForumListingModel
+            {
+                Id = forum.Id,
+                ImageUrl = forum.ImageUrl,
+                Name = forum.Title,
+                Description = forum.Description
+            };
+        }
+
         [HttpPost]
         public IActionResult Search(string searchQuery)
         {
